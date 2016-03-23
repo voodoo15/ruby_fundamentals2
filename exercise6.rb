9a96d4dea5f522e15c3a716980cbc2a41d0674f3
Arnold Redoblado <arnold.redoblado@gmail.com>
2016-03-23 12:59:27 -0400

    Formatted a bit more in Exercise 6

--- exercise6.rb
+++ exercise6.rb
@@ -10,7 +10,7 @@ grocery_items = [
       "toilet paper",
       "apples",
       "salmon"
-    ]
+    ].sort! #Sorted alphabetically in advance
 
 while to_exit == false
 
@@ -26,54 +26,40 @@ while to_exit == false
         item = gets.chomp
         grocery_items.push(item.to_s)
         grocery_items.sort!
-        puts "Grocery item added.  You need to pickup #{item.to_s}."
+        puts "Grocery item added.  You need to pickup #{ item.to_s }."
 
-        #Display groery items in array with asterisk
+      #Display groery items in array with asterisk
       when "i"
         grocery_items.sort!
-        grocery_items.each do |x|
-          puts "* #{x}"
+        grocery_items.each do | x |
+          puts "* #{ x }"
         end
 
       #Search for item and delete it if exists
       when "d"
         puts "Enter grocery item:"
         item = gets.chomp
-        grocery_items.delete_if{ |list_item| list_item == item.to_s }
+        grocery_items.delete_if{ | list_item | list_item == item.to_s }
         puts "Grocery item deleted."
 
       #Display a count of how many items are in the grocery list
       when "c"
-        puts "The number items in the grocery list is:  #{grocery_items.count}"
+        puts "The number items in the grocery list is:  #{ grocery_items.count }"
 
       #Check if an item is in the grocery list and display if item is in the list or not
       when "h"
         puts "Enter grocery item:"
         item = gets.chomp
 
-        #Flag if item exists from user prompt
-        item_check_flag = false
-        grocery_items.each { |item_check|
-          item_check_flag = true if item_check == item
-        }
-
-        #Display to user if item is needed or not
-        if item_check_flag == true
-          puts "Pick up #{item.to_s}."
-        else
-          puts "You don't need to pick up #{item.to_s}."
-        end
+        #If item exits or not, tell user
+        grocery_items.include?( item ) ? ( puts "Pick up #{ item.to_s }." ) : ( puts "You don't need to pick up #{ item.to_s }." )
 
         #Display which item is in the list if prompted
       when "w"
-        puts "Which item do you want to see:  "
+        puts "Which item do you want to see (enter number):  "
         item = gets.chomp
 
-        if item.to_i <= grocery_items.count
-          puts "The item in position #{item} is:  #{grocery_items[item.to_i - 1]}"
-        else
-          puts "The position is not avaiable in the list."
-        end
+        item.to_i <= grocery_items.count ? ( puts "The item in position #{ item } is:  #{ grocery_items[item.to_i - 1] }" ) : ( puts "The position is not avaiable in the list." )
 
         #Turn on flag to exit loop and end application
       when "x"
